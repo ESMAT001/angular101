5134beecdb4a59beeb67c5e5d3a4f8c63da91f79
ESMAT001 <esmat10.niazi@gmail.com>
2021-06-08 19:03:32 +0430

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestAppComponent } from './test-app/test-app.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestAppComponent,
     TestComponent
   ],
   imports: [
